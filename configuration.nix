7da3507bf15898eba77365d730132636c10d5245
sam brysbaert <sam.brysbaert@gmail.com>
2019-10-25 12:25:14 +0200

    enabled ssh

--- configuration.nix
+++ configuration.nix
@@ -133,6 +133,9 @@ in
   # enable trim to aid SSD performance;
   services.fstrim.enable = true;
 
+  # enable ssh
+  services.sshd.enable = true;
+
   # This value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
