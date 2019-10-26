fce5e7f42abf6e874be4708c3d7c85d5661a83b5
sam brysbaert <sam.brysbaert@gmail.com>
2019-10-26 16:32:18 +0200

    disabled dhcpcd because it is unnecessary if networkmanager is enabled

--- configuration.nix
+++ configuration.nix
@@ -63,15 +63,11 @@ in
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # enable networkmanager
-  networking.networkmanager.enable = true;
-
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
-  networking.useDHCP = false;
-  networking.interfaces.enp5s0.useDHCP = true;
-  networking.interfaces.wlp4s0.useDHCP = true;
+  # network settings
+  networking = {
+    networkmanager.enable = true;
+    dhcpcd.enable = false;
+  };
 
   # Set your time zone.
   time.timeZone = "Europe/Brussels";
